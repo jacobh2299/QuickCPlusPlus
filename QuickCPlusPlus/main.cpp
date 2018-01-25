6768daf341a0f79455fc68641825a294681afca1
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2018-01-25 08:17:24 -0700

    Initial Commit

---
+++ QuickCPlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuickCPlusPlus
+//
+//  Created by Harris, Jacob on 1/25/18.
+//  Copyright © 2018 Harris, Jacob. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
